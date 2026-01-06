d92b46266e28bd33b960d822001da0443b7333e4
Pranav Chengappa <pranavchengappa@Mac.fritz.box>
2026-01-06 17:44:45 +0100

    Forced dynamic home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import React from 'react'
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
